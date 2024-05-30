fa93210ade0ce3f5a6b8b6d4dead93485fa52b7b
EugeneGorbulya <gorbul04@yandex.ru>
2024-05-30 15:22:19 +0300

    updateChooseDb

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QCoreApplication::setOrganizationName("FlarRepair");
+    QCoreApplication::setApplicationName("Count");
+
+    QSettings settings;
+
+    QString filePathWorksDbPath = "data/BD.main.parametrs.project1.csv";
+    settings.setValue("worksDbPath", filePathWorksDbPath);
+    QString filePathMaterialsDbPath= "data/BD.work.for.project1.csv";
+    settings.setValue("materialsDbPath", filePathMaterialsDbPath);
+
     MainWindow w;
     w.show();
     return a.exec();
